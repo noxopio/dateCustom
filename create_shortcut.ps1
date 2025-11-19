41aa869ef926cbc8e0daccf8e111eaf68e21c4d4
nox <dark.tempus@gmail.com>
2025-11-19 09:46:23 -0500

    feat: Correcion de instalacion

--- create_shortcut.ps1
+++ create_shortcut.ps1
@@ -4,18 +4,23 @@
 
 $WshShell = New-Object -ComObject WScript.Shell
 $desktop = [Environment]::GetFolderPath('Desktop')
-$shortcutPath = Join-Path $desktop 'setDate'
+# El nombre completo del acceso directo debe terminar en .lnk o .url
+$shortcutPath = Join-Path $desktop 'setDate.lnk'
 
 # Detectar la carpeta donde está este script
 $scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
 $target = 'C:\Windows\System32\cmd.exe'
-$args = "/k cd /d `"$scriptPath`" && setDate_quick.bat"
+$shortcutArgs = "/k cd /d `"$scriptPath`" && setDate_quick.bat"
 
-$shortcut = $WshShell.CreateShortcut($shortcutPath)
-$shortcut.TargetPath = $target
-$shortcut.Arguments = $args
-$shortcut.WorkingDirectory = $scriptPath
-$shortcut.IconLocation = 'C:\Windows\System32\cmd.exe,0'
-$shortcut.Save()
-
-Write-Output "Acceso directo creado: $shortcutPath"
+try {
+	$shortcut = $WshShell.CreateShortcut($shortcutPath)
+	$shortcut.TargetPath = $target
+	$shortcut.Arguments = $shortcutArgs
+	$shortcut.WorkingDirectory = $scriptPath
+	$shortcut.IconLocation = 'C:\Windows\System32\cmd.exe,0'
+	$shortcut.Save()
+	Write-Output "Acceso directo creado: $shortcutPath"
+}
+catch {
+	Write-Error "No se pudo crear el acceso directo: $_"
+}
